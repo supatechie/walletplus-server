08e6235724b6a978acd56282f5c00c4679d8c085
Supatechie <torver.kelvin@gmail.com>
2021-09-22 22:48:58 +0100

    =remove dist dir

--- dist/validators/common.validator.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.validateUserId = void 0;
-const express_validator_1 = require("express-validator");
-exports.validateUserId = [
-    express_validator_1.param('id').trim().not().isEmpty().withMessage('User id is required'),
-];
--- dist/validators/transaction.validator.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.validateTransactionId = void 0;
-const express_validator_1 = require("express-validator");
-exports.validateTransactionId = [
-    express_validator_1.param('id').trim().not().isEmpty().withMessage('Transaction id is required'),
-];
